5f8c75d7b1de268c1891c141385774c4ed2ebfda
2002GOTOU <2002t7024@gmail.com>
2022-05-26 13:32:05 +0900

    初めてのコミット

---
+++ Assets/Bumper.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bumper : MonoBehaviour
+{
+    public float bounce = 10f;
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ball")
+        {
+            collision.rigidbody.AddForce(0f, bounce , bounce * 2, ForceMode.Impulse);
+        }
+    }
+}
---
+++ Assets/Rotation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour
+{
+    public float rotAngle = 4.0f;
+    private void FixedUpdate()
+    {
+        transform.Rotate(0f, rotAngle, 0f);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
